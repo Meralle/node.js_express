9dcfc2ca095a50f761a2a1f70c59785d6be2884b
Meralle <mar.ha17@outlook.com>
2018-05-23 12:02:53 +0200

    modified code

--- app.js
+++ app.js
@@ -5,24 +5,22 @@ var fs = require('fs');
 var file = fs.readFileSync('./events.json')
 
 var obj = JSON.parse(file);
- 
-
-
 
 // app.get('/', (req, res) => res.send('Home!'))
 app.get('/:id', (req, res) =>{
 
-	res.send(obj.filter(o => o._id['$oid'] ==req.params.id))
+	res.send(obj.filter(o => o._id['$oid'] == req.params.id))
 	console.log(req.params)
 })
 
- app.get('/', (req, res) => res.send(obj))// obj.filter(o => o.aqm.r3000['$numberInt'] > 70 )
- // console.log(req.query.apm)
+ app.get('/', (req, res) =>{
+ 	res.send(obj.filter(o => o.aqm.r3000['$numberInt'] > 70 ))
+  console.log(req.query.aqm)
+ })
  
-
 app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'))
 
-console.log('ehehhe')
+
 
  
    
