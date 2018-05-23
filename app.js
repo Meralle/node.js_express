decd5708222707943809900afcc1d8a600570756
Meralle <mar.ha17@outlook.com>
2018-05-23 14:06:41 +0200

    extend to three routes + styled

--- app.js
+++ app.js
@@ -7,16 +7,26 @@ var file = fs.readFileSync('./events.json')
 var obj = JSON.parse(file);
 
 // app.get('/', (req, res) => res.send('Home!'))
-app.get('/:id', (req, res) =>{
+app.get('/', (req, res) => {
+ 	res.sendFile(__dirname + '/index.html');
+  
+ })
+
+app.get('/events', (req, res) => {
+	// res.send(obj.filter(o => o.aqm.r3000['$numberInt'] > 70 ))
+	res.send(obj)
+	console.log(req.query.aqm)
+	
+
+})
+
+app.get('/events/:id', (req, res) =>{
 
 	res.send(obj.filter(o => o._id['$oid'] == req.params.id))
 	console.log(req.params)
 })
 
- app.get('/', (req, res) =>{
- 	res.send(obj.filter(o => o.aqm.r3000['$numberInt'] > 70 ))
-  console.log(req.query.aqm)
- })
+
  
 app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'))
 
