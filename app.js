dfd7661908a0028607e237921cc2b34c43fde1fc
Meralle <mar.ha17@outlook.com>
2018-05-23 11:51:23 +0200

    installed dotenv + express + nodemon + from events.json filtered obj + create home + id routing

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+require('dotenv').config();
+var fs = require('fs');
+var file = fs.readFileSync('./events.json')
+
+var obj = JSON.parse(file);
+ 
+
+
+
+// app.get('/', (req, res) => res.send('Home!'))
+app.get('/:id', (req, res) =>{
+
+	res.send(obj.filter(o => o._id['$oid'] ==req.params.id))
+	console.log(req.params)
+})
+
+ app.get('/', (req, res) => res.send(obj))// obj.filter(o => o.aqm.r3000['$numberInt'] > 70 )
+ // console.log(req.query.apm)
+ 
+
+app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'))
+
+console.log('ehehhe')
+
+ 
+   
+ 
