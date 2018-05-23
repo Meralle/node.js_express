9f20f96dfbada58456efe57c1eaf43028301e766
Meralle <mar.ha17@outlook.com>
2018-05-23 14:33:17 +0200

    delete css file

--- app.js
+++ app.js
@@ -8,30 +8,24 @@ var obj = JSON.parse(file);
 
 // app.get('/', (req, res) => res.send('Home!'))
 app.get('/', (req, res) => {
- 	res.sendFile(__dirname + '/index.html');
-  
- })
+    res.sendFile(__dirname + '/index.html');
+
+})
 
 app.get('/events', (req, res) => {
-	// res.send(obj.filter(o => o.aqm.r3000['$numberInt'] > 70 ))
-	res.send(obj)
-	console.log(req.query.aqm)
-	
+    // res.send(obj.filter(o => o.aqm.r3000['$numberInt'] > 70 ))
+    res.send(obj)
+    console.log(req.query.aqm)
+
 
 })
 
-app.get('/events/:id', (req, res) =>{
+app.get('/events/:id', (req, res) => {
 
-	res.send(obj.filter(o => o._id['$oid'] == req.params.id))
-	console.log(req.params)
+    res.send(obj.filter(o => o._id['$oid'] == req.params.id))
+    console.log(req.params)
 })
 
 
- 
+
 app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'))
-
-
-
- 
-   
- 
